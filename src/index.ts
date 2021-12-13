4126ac20dbf8d6d48457870ec9c62eb286558521
Andy Richardson <andy.john.richardson@gmail.com>
2021-12-13 15:10:54 +0000

    Parsing document

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
+import { parse } from "graphql";
+
 type WorkerEnv = { RESPONSES: KVNamespace; TYPENAMES: KVNamespace };
 
-const DESTINATION = "https://some-endpoint.com/graphql";
+const DESTINATION = "https://fakeql.com/graphql/dc08e1b5d4eff78fd7f7e9deb656444f";
 
 const fetchFn: ExportedHandlerFetchHandler<WorkerEnv> = async (
   request,
   env,
   ctx
 ) => {
-  return new Response();
+  const body = await request
+  .clone()
+  .text()
+  .then(JSON.parse);
+
+  const document = parse(body.query);
+
+  console.log(document)
+  // return null as any;
+  return fetch(new Request(DESTINATION, request));
 };
 
 export default {
